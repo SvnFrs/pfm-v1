e7319fea0c95656d88ae0c17fdea40f5235569fe
“SvnFrs” <thaidvqce170410@fpt.edu.vn>
2024-01-11 23:23:28 +0700

    Second commit

--- main.c
+++ main.c
@@ -1,20 +1,15 @@
 #include <stdio.h>
 #include <time.h>
 #include <cjson/cJSON.h>
+#include <stdlib.h>
 void printMenu();
-int fun(char date[]);
+// int fun(char date[]);
+void enterExpenses();
+void printDateChoice();
+char *getDate();
 int main()
 {
-    time_t rawtime;
-    struct tm *info;
-    char buffer[80];
-
-    time(&rawtime);
-
-    info = localtime(&rawtime);
-
-    strftime(buffer, 80, "%x - %I:%M%p", info);
-    printf("|%s|\n", buffer);
+    
     int choice, invalidChoice;
     do
     {
@@ -37,7 +32,7 @@ int main()
         {
         case 1:
             printf("Enter expenses\n");
-            fun(buffer);
+            enterExpenses();
             break;
         case 2:
             printf("List expenses\n");
@@ -67,25 +62,70 @@ void printMenu()
     printf("4. Exit\n");
 }
 
-int fun(char date[])
-{
-    printf("%s", date);
-    cJSON *json = cJSON_CreateObject();
-    cJSON_AddStringToObject(json, "Date", date);
-    char *json_str = cJSON_Print(json);
 
-    // write the JSON string to a file
-    FILE *fp = fopen("data/expenses.json", "w");
-    if (fp == NULL)
-    {
-        printf("Error: Unable to open the file.\n");
-        return 1;
-    }
-    printf("%s\n", json_str);
-    fputs(json_str, fp);
-    fclose;
-    // free the JSON string and cJSON object
-    cJSON_free(json_str);
-    cJSON_Delete(json);
-    return 0;
+void enterExpenses() {
+    printDateChoice();
+
 }
+void printDateChoice() {
+    printf("What day should it be?\n");
+    printf("1. Today\n");
+    printf("2. Other\n");
+    printf("Your choice:");
+    int choice;
+    scanf("%d", &choice);
+    switch (choice)
+    {
+    case 1:
+        char *date = getDate();
+        printf("Today is %s\n", date);
+        break;
+    case 2:
+        printf("Other\n");
+        break;
+    default:
+        printf("Invalid choice\n");
+        break;
+    }
+}
+
+char *getDate() {
+    time_t rawtime;
+    struct tm *info;
+    char *buffer = (char *)malloc(80 * sizeof(char)); // Allocate dynamic memory
+
+    if (buffer == NULL) {
+        fprintf(stderr, "Memory allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    time(&rawtime);
+    info = localtime(&rawtime);
+    strftime(buffer, 80, "%x", info);
+
+    return buffer;
+}
+
+
+// int fun(char date[])
+// {
+//     printf("%s", date);
+//     cJSON *json = cJSON_CreateObject();
+//     cJSON_AddStringToObject(json, "Date", date);
+//     char *json_str = cJSON_Print(json);
+
+//     // write the JSON string to a file
+//     FILE *fp = fopen("data/expenses.json", "w");
+//     if (fp == NULL)
+//     {
+//         printf("Error: Unable to open the file.\n");
+//         return 1;
+//     }
+//     printf("%s\n", json_str);
+//     fputs(json_str, fp);
+//     fclose;
+//     // free the JSON string and cJSON object
+//     cJSON_free(json_str);
+//     cJSON_Delete(json);
+//     return 0;
+// }
