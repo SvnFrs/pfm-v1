0d444a2d06e7ee58cb6a2654d9d696652da1250b
“SvnFrs” <thaidvqce170410@fpt.edu.vn>
2024-01-24 00:38:04 +0700

    Change the amount from char to long

--- testSave.h
+++ testSave.h
@@ -1,1 +1,1 @@
-extern int testSave(char day[80], char month[80], char year[80], char category[80], char description[80], char amount[80]);
+extern int testSave(char day[80], char month[80], char year[80], char category[80], char description[80], long amount);
