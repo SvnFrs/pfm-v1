a25a03e5c73fd2ea4ac3e89922d8bf53bd3082a0
“SvnFrs” <thaidvqce170410@fpt.edu.vn>
2024-02-23 16:04:05 +0700

    Move obsolete codes to another table file

--- table.h
+++ table.h
@@ -1,18 +1,8 @@
-#include <cjson/cJSON.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
-extern int listExpenses();
-extern void printAllExpenses();
-extern void printMonthlyExpenses();
-extern void printQuarterlyExpenses();
-extern void printYearlyExpenses();
-extern void printCustomExpenses();
 extern void createTableHeader(int columnCount, char *columnNames[], int columnWidths[]);
-extern void createTableBodyAll(cJSON *root, int columnCount, char *columnNames[], int columnWidths[]);
-extern void createTableBodyMonthly(cJSON *yearObj, cJSON *monthObj, int columnCount, char *columnNames[], int columnWidths[]);
-extern void createTableBodyQuarterly(cJSON *yearObj, int startMonth, int endMonth, int columnCount, char *columnNames[], int columnWidths[]);
-extern void createTableBodyYearly(cJSON *yearObj, int columnCount, char *columnNames[], int columnWidths[]);
-extern bool createTableBodyCustom(cJSON *root, int columnCount, char *columnNames[], int columnWidths[]);
 extern void createTableSeparator(int columnCount, int columnWidths[]);
-extern int* calculateColumnWidths(int columnCount, char *columnNames[], int columnWidths[]);
-extern int printQuarterlyChoice();
-extern void separateSEDate();
+extern int *calculateColumnWidths(int columnCount, char *columnNames[], int columnWidths[]);
