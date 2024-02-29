606c0efd85c06b881e9b58a2ebcea403ebb4c449
“SvnFrs” <thaidvqce170410@fpt.edu.vn>
2024-03-01 00:17:37 +0700

    Make validating public

---
+++ validate.c
@@ -0,0 +1,35 @@
+#include "validate.h"
+
+bool validateDate(char date[]) {
+    int day, month, year;
+    char *token = strtok(date, "/");
+    int count = 0;
+    while (token != NULL) {
+        if (count == 0) {
+            day = atoi(token);
+        } else if (count == 1) {
+            month = atoi(token);
+        } else if (count == 2) {
+            year = atoi(token);
+        }
+        token = strtok(NULL, "/");
+        count++;
+    }
+
+    if (day < 1 || day > 31) {
+        printf("Invalid day.\n");
+        return false;
+    }
+
+    if (month < 1 || month > 12) {
+        printf("Invalid month.\n");
+        return false;
+    }
+
+    if (year < 1970 || year > 2024) {
+        printf("Invalid year.\n");
+        return false;
+    }
+
+    return true;
+}
---
+++ validate.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <cjson/cJSON.h>
+
+extern bool validateDate(char date[]);
